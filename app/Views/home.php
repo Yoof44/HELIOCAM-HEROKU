b03f4842d961cf403b11f8df0d4472f469dada63
yoof25 <ealto.k12046049@umak.edu.ph>
2025-05-27 13:29:26 +0800

    testing auto deployment

--- app/Views/home.php
+++ app/Views/home.php
@@ -2,7 +2,7 @@
 <html class="scroll-smooth" lang="en" dir="ltr">
 
 <head>
-    <title>Test</title>
+    <title>HelioCam</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Updated favicon with proper path using base_url -->
